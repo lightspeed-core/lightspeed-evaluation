5a0b34012bd244aa02ccdb184c83aa60bea90290
Asutosh Samal <asamal@redhat.com>
2025-11-06 18:59:27 +0530

    integrate keywords eval

--- src/lightspeed_evaluation/core/metrics/custom/__init__.py
+++ src/lightspeed_evaluation/core/metrics/custom/__init__.py
@@ -1,6 +1,7 @@
 """Custom metrics components package."""
 
 from lightspeed_evaluation.core.metrics.custom.custom import CustomMetrics
+from lightspeed_evaluation.core.metrics.custom.keywords_eval import evaluate_keywords
 from lightspeed_evaluation.core.metrics.custom.prompts import (
     ANSWER_CORRECTNESS_PROMPT,
     INTENT_EVALUATION_PROMPT,
@@ -9,6 +10,7 @@ from lightspeed_evaluation.core.metrics.custom.tool_eval import evaluate_tool_ca
 
 __all__ = [
     "CustomMetrics",
+    "evaluate_keywords",
     "evaluate_tool_calls",
     # Prompts
     "ANSWER_CORRECTNESS_PROMPT",
