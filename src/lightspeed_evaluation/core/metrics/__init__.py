cc0dc60e34e2b95c6f80a0882909f2a3eee644d8
arin-deloatch <arindeloatch@gmail.com>
2025-11-07 15:15:00 -0700

    feat: added GEval handler; integrated component into DeepEvalMetrics

--- src/lightspeed_evaluation/core/metrics/__init__.py
+++ src/lightspeed_evaluation/core/metrics/__init__.py
@@ -5,4 +5,9 @@ from lightspeed_evaluation.core.metrics.deepeval import DeepEvalMetrics
 from lightspeed_evaluation.core.metrics.ragas import RagasMetrics
 from lightspeed_evaluation.core.metrics.script import ScriptEvalMetrics
 
-__all__ = ["RagasMetrics", "DeepEvalMetrics", "CustomMetrics", "ScriptEvalMetrics"]
+__all__ = [
+    "RagasMetrics",
+    "DeepEvalMetrics",
+    "CustomMetrics",
+    "ScriptEvalMetrics",
+]
