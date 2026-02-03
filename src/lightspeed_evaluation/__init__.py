092de4a4ce0076028b4e181e3238e9830a6a700b
Asutosh Samal <asamal@redhat.com>
2026-02-03 17:27:09 +0530

    bump up eval tool version (#152)

--- src/lightspeed_evaluation/__init__.py
+++ src/lightspeed_evaluation/__init__.py
@@ -39,7 +39,7 @@ if TYPE_CHECKING:
     )
     from lightspeed_evaluation.pipeline.evaluation import EvaluationPipeline
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 _LAZY_IMPORTS = {
     # Main pipeline
